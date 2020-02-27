d2d3627cf00c9a85501520afda9ad1c252dbf585
Michael Ingeman-Nielsen <min@praqma.net>
2020-02-27 14:47:57 +0100

    enable support for several kinds of setup in utils.sh

--- .shared/utils.sh
+++ .shared/utils.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
 
-# Initializes a kata
-# $1 = kata build.gradle contents
-initkata() {
-    echo "[KATA] Setting up the kata"
-    echo "[KATA] ------"
-    echo "[KATA] "
-
+cleanup() {
     echo "[KATA] Cleaning up old exercise"
     rm -rf exercise/
+}
 
+create_exercise_dir() {
     echo "[KATA] Initializing new exercise"
     mkdir exercise
     cd exercise
-    gradle -q init
-    echo "$1" > build.gradle
+}
 
+initialize_gradle_project() {
+    gradle -q init
+}
+
+print_header() {
+    echo "[KATA] Setting up the kata"
+    echo "[KATA] ------"
+    echo "[KATA] "   
+}
+
+print_footer() {
     echo "[KATA] Done!"
-    echo "[KATA] "
-    echo "[KATA] Don't forget to:"
-    echo "[KATA] 'cd exercise/'"
+}
+
+#######################################
+# Create a kata exercise folder and 
+# initialize gradle with build.gradle contents
+# Arguments:
+#   $1 = kata build.gradle contents
+#######################################
+function initkata() {
+    print_header
+
+    cleanup
+    create_exercise_dir
+    initialize_gradle_project
+    
+    echo "$1" > build.gradle
+    
+    print_footer
+}
+
+
+#######################################
+# Initialize a kata exercise folder
+# without contents
+# Arguments:
+#   None
+#######################################
+initkata_basic() {
+    print_header
+    
+    cleanup
+    create_exercise_dir
+
+    print_footer
 }
